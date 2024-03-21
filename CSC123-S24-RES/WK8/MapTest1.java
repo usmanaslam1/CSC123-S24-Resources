efa024c1f22a9be69122a7312d838d8517efc0cc
usman <usman@localhost>
2024-03-21 01:26:51 -0700

    Maps

---
+++ CSC123-S24-RES/WK8/MapTest1.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.text.CollationElementIterator;
+import java.util.ArrayList;
+
+public class MapTest1 {
+	
+
+	public static void main(String[] args) {
+		
+		//Create map to store information about a car
+		
+		Map<String, String> car= new HashMap<String, String>();
+		
+		//add information to the map
+		car.put("Make", "Toyota");
+		car.put("Model", "Camry");
+		car.put("Year", "2018");
+		car.put("Color", "Blue");
+		car.put("Miles", "10000");
+		
+		//get information from the map
+		car.get("Make");
+		
+		//remove information from the map
+		//car.remove("Make");
+		
+		//returns the size of the map
+		car.size();
+		
+		//returns true if the map is empty
+		car.isEmpty();
+		
+		//returns true if the map contains the key
+		car.containsKey("Make");
+		
+		//returns true if the map contains the value
+		car.containsValue("Toyota");
+		
+		//returns a set of the keys
+		
+		//System.out.println(car.containsKey("Color"));
+		
+		//System.out.println(car);
+
+		//Obtain all the keys from the map
+		Set<String> keys=car.keySet();
+		
+		//Iterate through the keys and print each key
+//		for (String key : keys) {
+//			System.out.println(key + " : " + car.get(key));
+//		}
+		
+		
+		Collection<String> values = car.values();
+		
+		for (String value : values) {
+			System.out.println(value);
+		}
+		
+		
+	}
+
+}
---
+++ CSC123-S24-RES/WK8/MapTest2.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.text.CollationElementIterator;
+import java.util.ArrayList;
+
+public class MapTest2 {
+	
+
+	public static void main(String[] args) {
+		
+		//Create map to store information about a car
+		
+		Map<String, String> car= new HashMap<String, String>();
+		ArrayList<Map> garage = new ArrayList<Map>();
+		
+		//add information to the map
+		car.put("Make", "Toyota");
+		car.put("Model", "Camry");
+		car.put("Year", "2018");
+		car.put("Color", "Blue");
+		car.put("Miles", "10000");
+		car.put("Radio", "Digital Radio");
+
+		garage.add(car);
+		
+		car= new HashMap<String, String>();
+		car.put("Make", "Ford");
+		car.put("Model", "Focus");
+		car.put("Year", "2017");
+		car.put("Color", "Red");
+		car.put("Miles", "20000");
+		car.put("Radio", "Digital Radio");
+		
+		garage.add(car);
+		
+		car= new HashMap<String, String>();
+		car.put("Make", "Honda");
+		car.put("Model", "Accord");
+		car.put("Year", "2016");
+		car.put("Color", "Black");
+		car.put("Miles", "30000");
+		car.put("Radio", "Digital Radio");
+		
+		garage.add(car);
+		
+		
+		for (Map carMap : garage) {
+			System.out.println(carMap);
+		}
+		
+	
+		
+	}
+
+}
